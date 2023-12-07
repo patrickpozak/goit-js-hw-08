8804a328f0cddad32384e58554e21c9fad7270a1
Patryk Pożak <pozak.p@gmail.com>
2023-12-07 22:36:53 +0100

    Task 1 Finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+const images = galleryItems.map(item => {
+  return `<li class="gallery__item">
+    <a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+    </li>`;
+});
+
+galleryList.insertAdjacentHTML('beforeend', images.join(''));
+
+galleryList.addEventListener('click', onClickGal);
+
+function onClickGal(e) {
+  e.preventDefault();
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  nav: false,
+  close: false,
+  scrollZoom: false,
+});
