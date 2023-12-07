8318a9643158e41bfaad962c2f93c2269c5b56ca
Patryk Pożak <pozak.p@gmail.com>
2023-12-08 00:38:27 +0100

    Task 2 Finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
 
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000)
+);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+});
